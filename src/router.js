cb9607a05cc4951434d2a567c8e6903efc5f76da
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-21 15:00:50 -0400

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,1 +1,11 @@
+import { createRouter } from 'vue-router'
+import Blog from './componets/Blog.vue'
 
+const router = createRouter({
+  history: true,
+  routes: [
+    { path: '/', componet: Blog }
+  ]
+})
+
+export default router
