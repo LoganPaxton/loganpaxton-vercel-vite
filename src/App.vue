204a72a6a8ad4c6facdfaa2157638f54954e15fd
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-22 10:24:49 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div>
-    <h1>LoganPaxton</h1>
+    <h1 id="NameH1">LoganPaxton</h1>
     <p>Hello! Welcome to my site! Here, I will have current projects, updates, and much more! Please, look around!</p>
   </div>
 </template>
