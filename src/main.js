b72f83332925939593e3e5f662e3092a72ef8dc2
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-22 10:18:25 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
-import Blog from './componets/Blog.vue'
 import './style.css'
 import App from './App.vue'
-import router from './router.js'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(App).mount('#app')
