ccf047e5378a7964349d535a05766f51784d7951
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-22 10:12:28 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueRouter from 'vue-router'
-
-export default defineConfig({
-  plugins: [vue(), vueRouter()],
-})
 
 // https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+})
