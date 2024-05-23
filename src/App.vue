e8f5ab04f581ba4d08ffd394fdd5b46885c3e0ae
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-23 18:57:06 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-  <div>
+  <div id="Welcome">
     <h1 id="NameH1">LoganPaxton</h1>
     <p>Hello! Welcome to my site! Here, I will have current projects, updates, and much more! Please, look around!</p>
+  </div>
+  <div id="Projects">
+    <h1>Projects</h1>
+    <a href="https://www.roblox.com/games/17386882104/Furniture-RNG-BETA">Furniture RNG (Roblox)</a>
+    <br>
+    <p>There may be projects that I don't list on here because they are in their pre-release stage!</p>
+  </div>
+  <div id="Updates">
+    <h1>Updates</h1>
+    <h2>Furniture RNG</h2>
+    <p>Furniture RNG may be open to the public this weekend! I am looking forward to it! (5/25/24)</p>
+    <p>More updates coming soon!</p>
   </div>
 </template>
 
